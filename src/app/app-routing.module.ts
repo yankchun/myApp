dd3e060e526019fc622298fba72e62a34a60862e
Nicholas <yankchun@hotmail.com>
2024-05-29 20:25:05 +0800

    code changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GeneralDetailsComponent } from './steps/general-details/general-details.component';
-import { PersonalDetailsComponent } from './steps/personal-details/personal-details.component';
-import { ReviewSubmitComponent } from './steps/review-submit/review-submit.component';
+import { GeneralDetailsComponent } from './components/general-details/general-details.component';
+import { PersonalDetailsComponent } from './components/personal-details/personal-details.component';
+import { ReviewSubmitComponent } from './components/review-submit/review-submit.component';
+import { CompleteComponent } from './components/complete/complete.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/general-details', pathMatch: 'full' },
   { path: 'general-details', component: GeneralDetailsComponent },
   { path: 'personal-details', component: PersonalDetailsComponent },
-  { path: 'review-submit', component: ReviewSubmitComponent }
+  { path: 'review-submit', component: ReviewSubmitComponent },
+  { path: 'complete', component: CompleteComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { GeneralDetailsComponent } from './steps/general-details/general-details.component';
-import { PersonalDetailsComponent } from './steps/personal-details/personal-details.component';
-import { ReviewSubmitComponent } from './steps/review-submit/review-submit.component';
+import { GeneralDetailsComponent } from './components/general-details/general-details.component';
+import { PersonalDetailsComponent } from './components/personal-details/personal-details.component';
+import { ReviewSubmitComponent } from './components/review-submit/review-submit.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { NgxLoadingModule } from 'ngx-loading';
+import { CompleteComponent } from './components/complete/complete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GeneralDetailsComponent,
     PersonalDetailsComponent,
-    ReviewSubmitComponent
+    ReviewSubmitComponent,
+    CompleteComponent
   ],
   imports: [
     BrowserModule,
